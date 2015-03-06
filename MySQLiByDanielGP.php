be81b0e08f02d31424208ff57d4924d0e7ae8f24
Daniel Popiniuc <danielpopiniuc@gmail.com>
2015-03-06 20:22:55 +0200

    removed reference to localization library from PHP within trait which means it will have to be added as requirement for each package using this library

--- MySQLiByDanielGP.php
+++ MySQLiByDanielGP.php
@@ -161,7 +161,6 @@ trait MySQLiByDanielGP
 
     private function handleLocalizationCommon()
     {
-        require_once '../vendor/inetsys/phpgettext/gettext.inc';
         if (isset($_GET['lang'])) {
             $_SESSION['lang'] = filter_var($_GET['lang'], FILTER_SANITIZE_STRING);
         } elseif (!isset($_SESSION['lang'])) {
