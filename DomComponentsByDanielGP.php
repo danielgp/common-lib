97ea8d31493eb1565e0ad37f791dd1963be2a9d9
Daniel Popiniuc <danielpopiniuc@gmail.com>
2015-03-22 19:22:29 +0200

    minor tweaking following a read of https://css-tricks.com/probably-use-initial-scale1/

--- DomComponentsByDanielGP.php
+++ DomComponentsByDanielGP.php
@@ -367,7 +367,7 @@ trait DomComponentsByDanielGP
             'lang'     => '<html lang="en-US">',
             'head'     => '<head>',
             'charset'  => '<meta charset="utf-8" />',
-            'viewport' => '<meta name="viewport" content="width=device-width" />',
+            'viewport' => '<meta name="viewport" content="width=device-width, initial-scale=1" />',
         ];
         $sReturn             = [];
         if (!is_null($headerFeatures)) {
