a320f0d5a2dbfa6b7d8dc30bc31ea71eb63a0a5c
Daniel Popiniuc <danielpopiniuc@gmail.com>
2015-04-20 23:29:23 +0300

    added a powerful function to output an Array into an Excel file (xlsx format, requires Microsoft Excel 2007 or newer or compatible software)

--- source/CommonCode.php
+++ source/CommonCode.php
@@ -82,7 +82,7 @@ trait CommonCode
             ];
             $aReturn['response'] = '';
         } else {
-            $aReturn['info']     = $this->setArray2json(curl_getinfo($ch));
+            $aReturn['info']     = $this->setArrayToJson(curl_getinfo($ch));
             $aReturn['response'] = $responseJsonFromClientOriginal;
         }
         curl_close($ch);
@@ -309,12 +309,168 @@ trait CommonCode
     }
 
     /**
+     * Generate an Excel file from a given array
+     *
+     * @param array $inFeatures
+     */
+    protected function setArrayToExcel($inFeatures)
+    {
+        if (!is_array($inFeatures['contentArray'])) {
+            echo 'no data!!!';
+        }
+        $xlFileName  = str_replace('.xls', '', $inFeatures['filename']) . '.xlsx';
+        // Create an instance
+        $objPHPExcel = new \PHPExcel();
+        // Set properties
+        if (isset($inFeatures['properties'])) {
+            if (isset($inFeatures['properties']['Creator'])) {
+                $objPHPExcel->getProperties()->setCreator($inFeatures['properties']['Creator']);
+            }
+            if (isset($inFeatures['properties']['LastModifiedBy'])) {
+                $objPHPExcel->getProperties()->setLastModifiedBy($inFeatures['properties']['LastModifiedBy']);
+            }
+            if (isset($inFeatures['properties']['description'])) {
+                $objPHPExcel->getProperties()->setDescription($inFeatures['properties']['description']);
+            }
+            if (isset($inFeatures['properties']['subject'])) {
+                $objPHPExcel->getProperties()->setSubject($inFeatures['properties']['subject']);
+            }
+            if (isset($inFeatures['properties']['title'])) {
+                $objPHPExcel->getProperties()->setTitle($inFeatures['properties']['title']);
+            }
+        }
+        // Add a worksheet to the file, returning an object to add data to
+        $objPHPExcel->setActiveSheetIndex(0);
+        if (is_array($inFeatures['contentArray'])) {
+            $counter = 0;
+            foreach ($inFeatures['contentArray'] as $key => $value) {
+                $columnCounter = 0;
+                if ($counter == 0) { // headers
+                    foreach ($value as $key2 => $value2) {
+                        $crCol          = $this->setArrayToExcelStringFromColumnIndex($columnCounter);
+                        $objPHPExcel->getActiveSheet()->getColumnDimension($crCol)->setAutoSize(true);
+                        $crtCellAddress = $crCol . '1';
+                        $objPHPExcel->getActiveSheet()->SetCellValue($crtCellAddress, $key2);
+                        $objPHPExcel->getActiveSheet()->getStyle($crCol . '1')->getFill()->applyFromArray([
+                            'type'       => 'solid',
+                            'startcolor' => ['rgb' => 'CCCCCC'],
+                            'endcolor'   => ['rgb' => 'CCCCCC'],
+                        ]);
+                        $objPHPExcel->getActiveSheet()->getStyle($crCol . '1')->applyFromArray([
+                            'font' => [
+                                'bold'  => true,
+                                'color' => ['rgb' => '000000'],
+                            ]
+                        ]);
+                        $columnCounter += 1;
+                    }
+                    $objPHPExcel->getActiveSheet()->calculateColumnWidths();
+                    $counter += 1;
+                }
+                $columnCounter = 0;
+                foreach ($value as $key2 => $value2) {
+                    if (strlen($value2) > 50) {
+                        $objPHPExcel->getActiveSheet()->getStyle($crtCellAddress)->getAlignment()->setWrapText(true);
+                    }
+                    if ($counter == 1) {
+                        $objPHPExcel->getActiveSheet()->getColumnDimension($crCol)->setAutoSize(false);
+                    }
+                    $crCol          = $this->setArrayToExcelStringFromColumnIndex($columnCounter);
+                    $crtCellAddress = $crCol . ($counter + 1);
+                    if (($value2 == '') || ($value2 == '00:00:00') || ($value2 == '0')) {
+                        $value2 = '';
+                    }
+                    if ((strlen($value2) == 8) && (strpos($value2, ':') !== false)) {
+                        if ($value2 == '') {
+                            $calculated_time_as_number = 0;
+                        } else {
+                            $calculated_time_as_number = $this->LocalTime2Seconds($value2) / 60 / 60 / 24;
+                        }
+                        $objPHPExcel->getActiveSheet()->SetCellValue($crtCellAddress, $calculated_time_as_number);
+                        $objPHPExcel
+                                ->getActiveSheet()
+                                ->getStyle($crtCellAddress)
+                                ->getNumberFormat()
+                                ->setFormatCode('[h]:mm:ss;@');
+                    } else {
+                        $objPHPExcel->getActiveSheet()->SetCellValue($crtCellAddress, strip_tags($value2));
+                    }
+                    $columnCounter += 1;
+                }
+                $counter += 1;
+            }
+            $objPHPExcel->setActiveSheetIndex(0);
+            $objPHPExcel->getActiveSheet()->setTitle($inFeatures['worksheetname']);
+            $objPHPExcel->getActiveSheet()->getPageSetup()->setOrientation('portrait');
+            //coresponding to A4
+            $objPHPExcel->getActiveSheet()->getPageSetup()->setPaperSize(9);
+            // freeze 1st top row
+            $objPHPExcel->getActiveSheet()->freezePane('A2');
+            // activate AutoFilter
+            $objPHPExcel->getActiveSheet()->setAutoFilter('A1:' . $crCol . ($counter - 1));
+            // margin is set in inches (0.7cm)
+            $margin    = 0.7 / 2.54;
+            $objPHPExcel->getActiveSheet()->getPageMargins()->setHeader($margin);
+            $objPHPExcel->getActiveSheet()->getPageMargins()->setTop($margin * 2);
+            $objPHPExcel->getActiveSheet()->getPageMargins()->setBottom($margin);
+            $objPHPExcel->getActiveSheet()->getPageMargins()->setLeft($margin);
+            $objPHPExcel->getActiveSheet()->getPageMargins()->setRight($margin);
+            // add header content
+            $objPHPExcel->getActiveSheet()->getHeaderFooter()->setOddHeader('&L&F&RPage &P / &N');
+            // repeat coloumn headings for every new page...
+            $objPHPExcel->getActiveSheet()->getPageSetup()->setRowsToRepeatAtTopByStartAndEnd(1, 1);
+            // activate printing of gridlines
+            $objPHPExcel->getActiveSheet()->setPrintGridlines(true);
+            // output the created content to the browser
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header('Pragma: private');
+            header('Cache-control: private, must-revalidate');
+            header('Content-Disposition: attachment;filename="' . $xlFileName . '"');
+            header('Cache-Control: max-age=0');
+            $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+            $objWriter->save('php://output');
+            unset($objPHPExcel);
+        }
+    }
+
+    /**
+     * Using a lookup cache adds a slight memory overhead,
+     * but boosts speed caching using a static within the method is faster than a class static,
+     * though it's additional memory overhead
+     *
+     * @staticvar array $_indexCache
+     * @param type $pColumnIndex
+     * @return string
+     */
+    private static function setArrayToExcelStringFromColumnIndex($pColumnIndex = 0)
+    {
+        //	Using a lookup cache adds a slight memory overhead, but boosts speed
+        //	caching using a static within the method is faster than a class static,
+        //	though it's additional memory overhead
+        static $_indexCache = [];
+        if (!isset($_indexCache[$pColumnIndex])) {
+            // Determine column string
+            if ($pColumnIndex < 26) {
+                $_indexCache[$pColumnIndex] = chr(65 + $pColumnIndex);
+            } elseif ($pColumnIndex < 702) {
+                $_indexCache[$pColumnIndex] = chr(64 + ($pColumnIndex / 26)) .
+                        chr(65 + $pColumnIndex % 26);
+            } else {
+                $_indexCache[$pColumnIndex] = chr(64 + (($pColumnIndex - 26) / 676)) .
+                        chr(65 + ((($pColumnIndex - 26) % 676) / 26)) .
+                        chr(65 + $pColumnIndex % 26);
+            }
+        }
+        return $_indexCache[$pColumnIndex];
+    }
+
+    /**
      * Converts an array into JSON string
      *
      * @param array $inArray
      * @return string
      */
-    protected function setArray2json($inArray)
+    protected function setArrayToJson($inArray)
     {
         if (!is_array($inArray)) {
             return 'Given input is not an array...';
