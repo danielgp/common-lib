31497b541d5d3ca5feaeb776cae748b647d98fa0
Daniel Popiniuc <danielpopiniuc@gmail.com>
2015-10-14 21:30:39 +0300

    just the license is properly reflected

--- js/tabber/tabber-management.js
+++ js/tabber/tabber-management.js
@@ -1,7 +1,7 @@
 /* 
  * The MIT License
  *
- * Copyright 2015 E303778.
+ * Copyright (c) 2006 Patrick Fitzgerald
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
