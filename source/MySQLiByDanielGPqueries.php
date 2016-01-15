7cd8ace7a3e2baf82f019c9ac12f68a82e88623f
Daniel Popiniuc <danielpopiniuc@gmail.com>
2016-01-15 23:44:47 +0100

    another split to keep the complexity to a lower value of MySQL queries trait

--- source/MySQLiByDanielGPqueries.php
+++ source/MySQLiByDanielGPqueries.php
@@ -36,18 +36,7 @@ namespace danielgp\common_lib;
 trait MySQLiByDanielGPqueries
 {
 
-    private function sCleanParameters(&$parameters)
-    {
-        if (is_array($parameters)) {
-            $tmpArray = [];
-            foreach ($parameters as &$value) {
-                $tmpArray[] = filter_var($value, FILTER_SANITIZE_STRING);
-            }
-            $parameters = $tmpArray;
-        } else {
-            $parameters = filter_var($parameters, FILTER_SANITIZE_STRING);
-        }
-    }
+    use MySQLiByDanielGPqueriesBasic;
 
     /**
      * Internal function to manage concatenation for filters
@@ -119,40 +108,6 @@ trait MySQLiByDanielGPqueries
         return '';
     }
 
-    /**
-     * Query to list Databases
-     *
-     * @param type $excludeSystemDbs
-     * @return type
-     */
-    protected function sQueryMySqlActiveDatabases($excludeSystemDbs = true)
-    {
-        $sDBs = 'WHERE '
-                . '`SCHEMA_NAME` NOT IN ("'
-                . implode('", "', ['information_schema', 'mysql', 'performance_schema', 'sys']) . '") ';
-        return 'SELECT '
-                . '`SCHEMA_NAME` As `Db`, `DEFAULT_CHARACTER_SET_NAME` AS `DbCharset`, '
-                . '`DEFAULT_COLLATION_NAME` AS `DbCollation` '
-                . 'FROM `information_schema`.`SCHEMATA` '
-                . ($excludeSystemDbs ? $sDBs : '')
-                . 'GROUP BY `SCHEMA_NAME`;';
-    }
-
-    /**
-     * Query to list MySQL engines
-     *
-     * @param string $onlyActiveOnes
-     * @return type
-     */
-    protected function sQueryMySqlActiveEngines($onlyActiveOnes = true)
-    {
-        return 'SELECT '
-                . '`ENGINE` AS `Engine`, `SUPPORT` AS `Support`, `COMMENT` AS `Comment` '
-                . 'FROM `information_schema`.`ENGINES` '
-                . ($onlyActiveOnes ? 'WHERE (`SUPPORT` IN ("DEFAULT", "YES")) ' : '')
-                . 'GROUP BY `ENGINE`;';
-    }
-
     protected function sQueryMySqlColumns($filterArray = null)
     {
         return 'SELECT '
@@ -169,36 +124,13 @@ trait MySQLiByDanielGPqueries
                 . 'ORDER BY `C`.`TABLE_SCHEMA`, `C`.`TABLE_NAME`, `C`.`ORDINAL_POSITION`;';
     }
 
-    protected function sQueryMySqlColumnsColumns()
+    private function sQueryMySqlColumnsColumns()
     {
         return '`C`.`TABLE_NAME`, `C`.`COLUMN_NAME`, `C`.`ORDINAL_POSITION` '
                 . ', `C`.`COLUMN_DEFAULT`, `C`.`IS_NULLABLE`, `C`.`DATA_TYPE`, `C`.`CHARACTER_MAXIMUM_LENGTH` '
                 . ', `C`.`NUMERIC_PRECISION`, `C`.`NUMERIC_SCALE`, `C`.`DATETIME_PRECISION` '
                 . ', `C`.`CHARACTER_SET_NAME`, `C`.`COLLATION_NAME`, `C`.`COLUMN_TYPE` '
                 . ', `C`.`COLUMN_KEY`, `C`.`COLUMN_COMMENT`, `C`.`EXTRA`';
-    }
-
-    protected function sQueryGenericSelectKeyValue($parameters)
-    {
-        $this->sCleanParameters($parameters);
-        return implode(' ', [
-            'SELECT',
-            $parameters[0] . ',' . $parameters[1],
-            'FROM',
-            $parameters[2],
-            'GROUP BY',
-            $parameters[1] . ';'
-        ]);
-    }
-
-    /**
-     * Query to list Global Variables
-     *
-     * @return string
-     */
-    protected function sQueryMySqlGlobalVariables()
-    {
-        return 'SHOW GLOBAL VARIABLES;';
     }
 
     /**
@@ -227,23 +159,13 @@ trait MySQLiByDanielGPqueries
                 . $this->xtraSoring($filterArray, 'COLUMN_NAME') . ';';
     }
 
-    protected function sQueryMySqlIndexesColumns()
+    private function sQueryMySqlIndexesColumns()
     {
         return '`KCU`.`CONSTRAINT_NAME`, `KCU`.`TABLE_SCHEMA`, `KCU`.`TABLE_NAME`, '
                 . '`KCU`.`COLUMN_NAME`, `C`.`ORDINAL_POSITION` AS `COLUMN_POSITION`, `KCU`.`ORDINAL_POSITION`, '
                 . '`KCU`.`POSITION_IN_UNIQUE_CONSTRAINT`, `KCU`.`REFERENCED_TABLE_SCHEMA`, '
                 . '`KCU`.`REFERENCED_TABLE_NAME`, `KCU`.`REFERENCED_COLUMN_NAME`, '
                 . '`RC`.`UPDATE_RULE`, `RC`.`DELETE_RULE`';
-    }
-
-    /**
-     * The MySQL server time
-     *
-     * @return string
-     */
-    protected function sQueryMySqlServerTime()
-    {
-        return 'SELECT NOW();';
     }
 
     private function sQueryMySqlStatisticPattern($tblName, $lnkDbCol, $adtnlCol = null, $adtnlFltr = null)
@@ -286,30 +208,7 @@ trait MySQLiByDanielGPqueries
                 . ', `T`.`CREATE_OPTIONS`, `T`.`TABLE_COMMENT` '
                 . 'FROM `information_schema`.`TABLES` `T` '
                 . $this->sManageDynamicFilters($filterArray, 'T')
-                . $this->xtraSoring($filterArray, 'TABLE_SCHEMA')
-                . ';';
-    }
-
-    /**
-     * Get all the row details from a table based on given filter
-     *
-     * @param array $parameters
-     * @return string
-     */
-    protected function sQueryRowsFromTable($parameters)
-    {
-        $this->sCleanParameters($parameters);
-        return 'SELECT * '
-                . 'FROM `' . $parameters[0] . '` '
-                . 'WHERE ' . $parameters[1] . ';';
-    }
-
-    protected function sQueryToDeleteSingleIdentifier($parameters)
-    {
-        $this->sCleanParameters($parameters);
-        return 'DELETE '
-                . 'FROM `' . $parameters[0] . '` '
-                . 'WHERE `' . $parameters[1] . '` = "' . $parameters[2] . '";';
+                . $this->xtraSoring($filterArray, 'TABLE_SCHEMA') . ';';
     }
 
     private function xtraSoring($filterArray, $filterValueToDecide)
---
+++ source/MySQLiByDanielGPqueriesBasic.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Daniel Popiniuc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+namespace danielgp\common_lib;
+
+/**
+ * Queries for the MySQL module
+ *
+ * @author Daniel Popiniuc
+ */
+trait MySQLiByDanielGPqueriesBasic
+{
+
+    private function sCleanParameters(&$parameters)
+    {
+        if (is_array($parameters)) {
+            $tmpArray = [];
+            foreach ($parameters as &$value) {
+                $tmpArray[] = filter_var($value, FILTER_SANITIZE_STRING);
+            }
+            $parameters = $tmpArray;
+        } else {
+            $parameters = filter_var($parameters, FILTER_SANITIZE_STRING);
+        }
+    }
+
+    protected function sQueryGenericSelectKeyValue($parameters)
+    {
+        $this->sCleanParameters($parameters);
+        return implode(' ', [
+            'SELECT',
+            $parameters[0] . ',' . $parameters[1],
+            'FROM',
+            $parameters[2],
+            'GROUP BY',
+            $parameters[1] . ';'
+        ]);
+    }
+
+    /**
+     * Query to list Databases
+     *
+     * @param type $excludeSystemDbs
+     * @return type
+     */
+    protected function sQueryMySqlActiveDatabases($excludeSystemDbs = true)
+    {
+        $sDBs = 'WHERE '
+                . '`SCHEMA_NAME` NOT IN ("'
+                . implode('", "', ['information_schema', 'mysql', 'performance_schema', 'sys']) . '") ';
+        return 'SELECT '
+                . '`SCHEMA_NAME` As `Db`, `DEFAULT_CHARACTER_SET_NAME` AS `DbCharset`, '
+                . '`DEFAULT_COLLATION_NAME` AS `DbCollation` '
+                . 'FROM `information_schema`.`SCHEMATA` '
+                . ($excludeSystemDbs ? $sDBs : '')
+                . 'GROUP BY `SCHEMA_NAME`;';
+    }
+
+    /**
+     * Query to list MySQL engines
+     *
+     * @param string $onlyActiveOnes
+     * @return type
+     */
+    protected function sQueryMySqlActiveEngines($onlyActiveOnes = true)
+    {
+        return 'SELECT '
+                . '`ENGINE` AS `Engine`, `SUPPORT` AS `Support`, `COMMENT` AS `Comment` '
+                . 'FROM `information_schema`.`ENGINES` '
+                . ($onlyActiveOnes ? 'WHERE (`SUPPORT` IN ("DEFAULT", "YES")) ' : '')
+                . 'GROUP BY `ENGINE`;';
+    }
+
+    /**
+     * Query to list Global Variables
+     *
+     * @return string
+     */
+    protected function sQueryMySqlGlobalVariables()
+    {
+        return 'SHOW GLOBAL VARIABLES;';
+    }
+
+    /**
+     * The MySQL server time
+     *
+     * @return string
+     */
+    protected function sQueryMySqlServerTime()
+    {
+        return 'SELECT NOW();';
+    }
+
+    /**
+     * Get all the row details from a table based on given filter
+     *
+     * @param array $parameters
+     * @return string
+     */
+    protected function sQueryRowsFromTable($parameters)
+    {
+        $this->sCleanParameters($parameters);
+        return 'SELECT * '
+                . 'FROM `' . $parameters[0] . '` '
+                . 'WHERE ' . $parameters[1] . ';';
+    }
+
+    protected function sQueryToDeleteSingleIdentifier($parameters)
+    {
+        $this->sCleanParameters($parameters);
+        return 'DELETE '
+                . 'FROM `' . $parameters[0] . '` '
+                . 'WHERE `' . $parameters[1] . '` = "' . $parameters[2] . '";';
+    }
+}
