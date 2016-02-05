665550eb52082d740417588f93ac780b400576dd
Daniel Popiniuc <danielpopiniuc@gmail.com>
2016-02-05 22:43:28 +0200

    further complexity reduction per trait

--- source/MySQLiByDanielGPstructures.php
+++ source/MySQLiByDanielGPstructures.php
@@ -39,6 +39,21 @@ trait MySQLiByDanielGPstructures
     use MySQLiByDanielGP;
 
     /**
+     * Ensures table has special quoes and DOT as final char
+     * (if not empty, of course)
+     *
+     * @param string $referenceTable
+     * @return string
+     */
+    private function correctTableWithQuotesAsFieldPrefix($referenceTable)
+    {
+        if ($referenceTable != '') {
+            return '`' . str_replace('`', '', $referenceTable) . '`.';
+        }
+        return '';
+    }
+
+    /**
      * Return the list of Tables from the MySQL server
      *
      * @return string
@@ -139,6 +154,90 @@ trait MySQLiByDanielGPstructures
     protected function getMySQLlistTables($filterArray = null)
     {
         return $this->getMySQLlistMultiple('Tables', 'full_array_key_numbered', $filterArray);
+    }
+
+    /**
+     * Return the time from the MySQL server
+     *
+     * @return string
+     */
+    protected function getMySQLserverTime()
+    {
+        return $this->getMySQLlistMultiple('ServerTime', 'value');
+    }
+
+    /**
+     * Reads data from table into REQUEST super global
+     *
+     * @param string $tableName
+     * @param array $filtersArray
+     */
+    protected function getRowDataFromTable($tableName, $filtersArray)
+    {
+        $query   = $this->sQueryRowsFromTable([$tableName, $this->setArrayToFilterValues($filtersArray)]);
+        $rawData = $this->setMySQLquery2Server($query, 'array_pairs_key_value')['result'];
+        if (!is_null($rawData)) {
+            $this->initializeSprGlbAndSession();
+            foreach ($rawData as $key => $value) {
+                $vToSet = str_replace(['\\\\"', '\\"', "\\\\'", "\\'"], ['"', '"', "'", "'"], $value);
+                $this->tCmnRequest->request->set($key, $vToSet);
+            }
+        }
+    }
+
+    /**
+     * Builds an filter string from pair of key and value, where value is array
+     *
+     * @param string $key
+     * @param array $value
+     * @param string $referenceTable
+     * @return string
+     */
+    private function setArrayLineArrayToFilter($key, $value, $referenceTable)
+    {
+        $filters2 = implode(', ', array_diff($value, ['']));
+        if ($filters2 != '') {
+            return '(' . $referenceTable . '`' . $key . '` IN ("'
+                    . str_replace(',', '","', str_replace(["'", '"'], '', $filters2)) . '"))';
+        }
+        return '';
+    }
+
+    /**
+     * Builds an filter string from pair of key and value, none array
+     *
+     * @param string $key
+     * @param int|float|string $value
+     * @return string
+     */
+    private function setArrayLineToFilter($key, $value)
+    {
+        $fTemp = '=';
+        if ((substr($value, 0, 1) == '%') && (substr($value, -1) == '%')) {
+            $fTemp = 'LIKE';
+        }
+        return '(`' . $key . '` ' . $fTemp . '"' . $value . '")';
+    }
+
+    /**
+     * Transforms an array into usable filters
+     *
+     * @param array $entryArray
+     * @param string $referenceTable
+     * @return array
+     */
+    private function setArrayToFilterValues($entryArray, $referenceTable = '')
+    {
+        $filters  = [];
+        $refTable = $this->correctTableWithQuotesAsFieldPrefix($referenceTable);
+        foreach ($entryArray as $key => $value) {
+            if (is_array($value)) {
+                $filters[] = $this->setArrayLineArrayToFilter($key, $value, $refTable);
+            } elseif (!in_array($value, ['', '%%'])) {
+                $filters[] = $this->setArrayLineToFilter($key, $value);
+            }
+        }
+        return implode(' AND ', array_diff($filters, ['']));
     }
 
     /**
