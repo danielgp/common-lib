fe6fe2db2604527e96efb3b0819e86ce102dcdff
Daniel Popiniuc <danielpopiniuc@gmail.com>
2018-07-01 13:55:05 +0300

    more code moving from 1 trait to another to improve complexity balance

--- source/MySQLiByDanielGPtables.php
+++ source/MySQLiByDanielGPtables.php
@@ -33,10 +33,103 @@ namespace danielgp\common_lib;
  *
  * @author Daniel Popiniuc
  */
-trait MySQLiByDanieGPtables
+trait MySQLiByDanielGPtables
 {
 
     use MySQLiByDanielGPstructures;
+
+    /**
+     * Returns a Numeric field 2 use in a form
+     *
+     * @param string $tblSrc
+     * @param array $value
+     * @param array $iar
+     * @return string
+     */
+    private function getFieldOutputNumeric($tblSrc, $value, $iar = [])
+    {
+        if ($value['EXTRA'] == 'auto_increment') {
+            return $this->getFieldOutputNumericAI($value, $iar);
+        }
+        $fkArray = $this->getForeignKeysToArray($this->advCache['workingDatabase'], $tblSrc, $value['COLUMN_NAME']);
+        if ($fkArray === []) {
+            $fldNos = $this->setFieldNumbers($value);
+            return $this->getFieldOutputTT($value, min(50, (array_key_exists('l', $fldNos) ? $fldNos['l'] : 99)), $iar);
+        }
+        return $this->getFieldOutputNumericNonFK($fkArray, $value, $iar);
+    }
+
+    /**
+     * Handles creation of Auto Increment numeric field type output
+     *
+     * @param array $value
+     * @param array $iar
+     * @return string
+     */
+    private function getFieldOutputNumericAI($value, $iar = [])
+    {
+        if ($this->getFieldValue($value) == '') {
+            $spF = ['id' => $value['COLUMN_NAME'], 'style' => 'font-style:italic;'];
+            return $this->setStringIntoTag('auto-numar', 'span', $spF);
+        }
+        $inAdtnl = [
+            'type'  => 'hidden',
+            'name'  => $value['COLUMN_NAME'],
+            'id'    => $value['COLUMN_NAME'],
+            'value' => $this->getFieldValue($value),
+        ];
+        if ($iar !== []) {
+            $inAdtnl = array_merge($inAdtnl, $iar);
+        }
+        return '<b>' . $this->getFieldValue($value) . '</b>' . $this->setStringIntoShortTag('input', $inAdtnl);
+    }
+
+    /**
+     * Builds field output type for numeric types if not FK
+     *
+     * @param array $fkArray
+     * @param array $value
+     * @param array $iar
+     * @return string
+     */
+    private function getFieldOutputNumericNonFK($fkArray, $value, $iar = [])
+    {
+        $query         = $this->sQueryGenericSelectKeyValue([
+            '`' . $value['COLUMN_NAME'] . '`',
+            $fkArray[$value['COLUMN_NAME']][2],
+            $fkArray[$value['COLUMN_NAME']][0],
+        ]);
+        $selectOptions = $this->setMySQLquery2Server($query, 'array_key_value')['result'];
+        $selectValue   = $this->getFieldValue($value);
+        $inAdtnl       = ['size' => 1];
+        if ($value['IS_NULLABLE'] == 'YES') {
+            $inAdtnl = array_merge($inAdtnl, ['include_null']);
+        }
+        if ($iar !== []) {
+            $inAdtnl = array_merge($inAdtnl, $iar);
+        }
+        return $this->setArrayToSelect($selectOptions, $selectValue, $value['COLUMN_NAME'], $inAdtnl);
+    }
+
+    /**
+     * Prepares the text output fields
+     *
+     * @param string $tbl
+     * @param array $value
+     * @return array
+     */
+    private function getFieldOutputTextPrerequisites($tbl, $value)
+    {
+        $foreignKeysArray = [];
+        if (($tbl != 'user_rights') && ($value['COLUMN_NAME'] != 'eid')) {
+            $database = $this->advCache['workingDatabase'];
+            if (strpos($tbl, '`.`')) {
+                $database = substr($tbl, 0, strpos($tbl, '`.`'));
+            }
+            $foreignKeysArray = $this->getForeignKeysToArray($database, $tbl, $value['COLUMN_NAME']);
+        }
+        return $foreignKeysArray;
+    }
 
     /**
      * Returns an array with fields referenced by a Foreign key
