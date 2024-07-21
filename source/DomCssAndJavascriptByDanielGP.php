14d0cfb6bfcbdd751b09d924eae4f92c192fc248
Daniel Popiniuc <danielpopiniuc@gmail.com>
2024-07-22 02:37:34 +0300

    up-to-date logic

--- source/DomCssAndJavascriptByDanielGP.php
+++ source/DomCssAndJavascriptByDanielGP.php
@@ -4,7 +4,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Daniel Popiniuc
+ * Copyright (c) 2015 - 2024 Daniel Popiniuc
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,8 +35,7 @@ namespace danielgp\common_lib;
  */
 trait DomCssAndJavascriptByDanielGP
 {
-
-    use \danielgp\browser_agent_info\BrowserAgentInfosByDanielGP,
+    use \danielgp\io_operations\InputOutputBrowserAgent,
         DomPaginationByDanielGP,
         DomCssAndJavascriptByDanielGPwithCDN;
 
